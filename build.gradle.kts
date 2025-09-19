f3bc5d4f5946be21706dbe8a76ff1660bbc236cd
Yongshuai Wang <debui4578@outlook.com>
2025-09-19 10:44:42 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,6 +111,11 @@ subprojects {
             }
         }
     }
+
+    dependencies {
+    implementation("io.github.libxposed:api-annotations:1.0.0") // Replace with latest version
+    }
+    
     plugins.withType(JavaPlugin::class.java) {
         extensions.configure(JavaPluginExtension::class.java) {
             sourceCompatibility = androidSourceCompatibility
